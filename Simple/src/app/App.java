31b0fe3bf70b94d15c72e0fde9203984a70e3ef6
blueram@andriywv <blueram@andriywv>
2024-05-19 13:00:15 -0400

    Added message in double rep

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World 2");
         System.out.println("More practice");
+        System.out.println("Mondo grass is the best cover grass");
 
     }
 }
